27840b68b9f739a2604a7ae19438a267adf68564
ibokalo <bokalo.ihor@gmail.com>
2019-12-13 09:47:51 +0200

    dasssxcasd

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -2,4 +2,4 @@
 
 ssh ubuntu@ec2-52-70-198-12.compute-1.amazonaws.com mkdir -p /var/www/temp_deploy
 scp -r dist ubuntu@ec2-52-70-198-12.compute-1.amazonaws.com:/var/www/temp_deploy/dist/
-ssh -i "/root/.ssh/test2222.pem" ubuntu@ec2-52-70-198-12.compute-1.amazonaws.com “rm -rf /var/www/example.com/dist/ && mv /var/www/temp_deploy/dist/ /var/www/example.com/
+ssh ubuntu@ec2-52-70-198-12.compute-1.amazonaws.com “rm -rf /var/www/example.com/dist/ && mv /var/www/temp_deploy/dist/ /var/www/example.com/
