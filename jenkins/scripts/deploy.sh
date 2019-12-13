251149b0a6338b6cdff83836966a76a0493bb158
ibokalo <bokalo.ihor@gmail.com>
2019-12-13 09:42:32 +0200

    dasssxcasd

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-sh 'ssh ubuntu@ec2-52-70-198-12.compute-1.amazonaws.com mkdir -p /var/www/temp_deploy'
-sh 'scp -r dist ubuntu@ec2-52-70-198-12.compute-1.amazonaws.com:/var/www/temp_deploy/dist/'
-sh 'ssh -i "/root/.ssh/test2222.pem" ubuntu@ec2-52-70-198-12.compute-1.amazonaws.com “rm -rf /var/www/example.com/dist/ && mv /var/www/temp_deploy/dist/ /var/www/example.com/'
+ssh ubuntu@ec2-52-70-198-12.compute-1.amazonaws.com mkdir -p /var/www/temp_deploy
+scp -r dist ubuntu@ec2-52-70-198-12.compute-1.amazonaws.com:/var/www/temp_deploy/dist/
+ssh -i "/root/.ssh/test2222.pem" ubuntu@ec2-52-70-198-12.compute-1.amazonaws.com “rm -rf /var/www/example.com/dist/ && mv /var/www/temp_deploy/dist/ /var/www/example.com/
